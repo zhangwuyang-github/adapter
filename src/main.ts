a8af86d6854956a5f03980e3ec626cac3f2c85d9
zhang.wuyang <zhang.wuyang@xinheyun.com>
2024-07-10 16:57:00 +0800

    feat: 响应头中增加ResponseTime接口响应时间

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
+import { Logger } from '@nestjs/common';
+import * as responseTime from 'response-time';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './filter/any-exception.filter';
 import { LoggerService } from './middleware/logger/logger.service';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.use(responseTime());
   await app.listen(9000, async () => {
     app.useLogger(app.get(LoggerService));
     const url = await app.getUrl();
